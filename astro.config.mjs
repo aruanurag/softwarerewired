658483aa41f2155cc9ade1a852491704ed0329cd
Anuragg Mohan <anurag.a.mohan@oracle.com>
2026-03-15 19:20:21 -0400

    Fix Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import rss from "@astrojs/rss";
-
 export default defineConfig({
   site: "https://softwarerewired.com",
   output: "static",
-  integrations: [rss()]
+  integrations: []
 });
